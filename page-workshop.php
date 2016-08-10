445a917399781f8b4ce011cd4670515aec3206f7
John Mazzucco <John.Mazzucco@gmail.com>
2016-08-10 17:38:54 -0400

    Copy html over to page-workshop.php

--- page-workshop.php
+++ page-workshop.php
@@ -1,62 +1,483 @@
-<?php
-/*
-Template Name: Workshop Page
-*/
 
-get_header(); ?>
-<?php acf_form_head(); ?>
 
-	<div id="primary" class="fp-content-area">
-		<main id="main" class="site-main" role="main">
 
-			<div class="entry-content">
-			<?php query_posts('category=workshops'); ?>
+<!--
+ *
+ * NEUROTECHX CHAPTER TEMPLATE
+ * based on Grayscale Bootstrap Theme and Singlepro Theme
+ *
+ * The Goal is to make a sleek, easily customizable template
+ * so that all NeuroTechX chapters have a fairly similar layout that ties in
+ * with the main NeuroTechX website.
+ *
+ * Standard categories so far: About (+ Mandates) ,Events, Projects, Team, Sponsors, International Community ( + Map, + Slack links ) , Contact(+social media). Meetup link could go way up on nav bar as well.
+ *
+ *
+ * Don't forget to customize your background images in the template.css file!
+-->
 
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php the_content(); ?>
+<!DOCTYPE html>
 
-					<?php the_field('workshop_title'); ?>
-					<?php the_field('workshop_date_start'); ?>
-					<?php the_field('workshop_time_start'); ?>
-					<?php the_field('workshop_date_end'); ?>
-					<?php the_field('workshop_time_end'); ?>
-					<?php the_field('workshop_entry'); ?>
-					<?php the_field('workshop_cost'); ?>
-					<?php the_field('workshop_level'); ?>
-					<?php the_field('workshop_location')?>
+<html lang="en">
 
-					<?php 
+<head>
 
-					$location = get_field('location');
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>NeuroTechEdu - The International Neurotech Network</title>
 
-					if( !empty($location) ):
-					?>
-					<div class="acf-map">
-						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-					</div>
-					<?php endif; ?>
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- 					<iframe
-					  width="600"
-					  height="450"
-					  frameborder="0" style="border:0"
-					  src="https://www.google.com/maps/embed/v1/view
-					  ?key=YOUR_API_KEY
-					  &center=<?php the_field('workshop_location')?>
-					  &zoom=18
-					  &maptype=satellite" allowfullscreen>
-					</iframe> -->
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
 
-					<?php the_field('workshop_contact'); ?>
-					<?php the_field('workshop_summary'); ?>
-					<?php the_field('workshop_requirements'); ?>
-					<?php the_field('workshop_program'); ?>
-					<?php the_field('workshop_instructor'); ?>
-					<?php the_field('workshop_sponsors'); ?>
-				<?php endwhile; ?>
-			</div><!-- .entry-content -->
+  <!-- CSS
+  ================================================== -->
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  <!--Equal Column sizes-->
+  <link href="css/columns.css" rel="stylesheet">
 
-<?php get_footer(); ?>
+  <!-- Font awesome css file-->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+
+  <!-- NeurotechX icon font file-->
+  <link href="css/neurotechx.css" rel="stylesheet">
+
+  <!-- Superslide css file-->
+  <link href="css/superslides.css" rel="stylesheet">
+
+  <!-- Slick slider css file -->
+  <link href="css/slick.css" rel="stylesheet">
+
+  <!-- smooth animate css file -->
+  <link href="css/animate.css" rel="stylesheet">
+
+  <!-- Elastic grid css file -->
+  <link href="css/elastic_grid.css" rel="stylesheet">
+
+  <!-- Circle counter cdn css file -->
+  <link href='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/css/jquery.circliful.css' rel='stylesheet prefetch'>
+
+  <!-- Default Theme css file -->
+  <link id="orginal" href="css/themes/default-theme.css" rel="stylesheet">
+
+  <!-- Custom Template CSS -->
+  <link href="css/template.css" rel="stylesheet">
+
+  <!-- Custom Sections CSS -->
+  <link href="css/custom-sections.css" rel="stylesheet">
+
+  <!-- Google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Orbitron:400,700" rel="stylesheet" type="text/css" >
+  <link href='https://fonts.googleapis.com/css?family=Aldrich:400' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet' type='text/css'>
+  <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+        <i class="fa fa-bars"></i>
+      </button>
+
+      <!-- Chapter Name and Logo -->
+      <a class="navbar-brand page-scroll" href="#page-top">
+       <i class="icon-neurotechx-vector"></i>
+       <span style="color:#fff; font-family:Orbitron">NeuroTech</span>
+       <span style="color:#23b3c6; font-family:Orbitron">X</span>
+      </a>
+
+    </div>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+
+        <li class="hidden">
+          <a href="#page-top"></a>
+        </li>
+
+        <li>
+          <a id="workshops-link" class="page-scroll" href="#workshops">Workshops</a>
+        </li>
+
+        <li>
+          <a id="about-us-link" class="page-scroll" href="http://neurotechx.com/#AboutUs">About us</a>
+        </li>
+
+        <li>
+          <a id="stay-tuned-link" class="page-scroll" href="http://eepurl.com/bEQDKX" target="_blank">Stay Tuned</a>
+        </li>
+
+        <li>
+          <a class="nav-icons" href="https://facebook.com/neurotechx" target="_blank">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+
+        <li>
+          <a class="nav-icons" href="https://twitter.com/neurotechx" target="_blank">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+
+        <li>
+          <a class="nav-icons" href="https://www.linkedin.com/company/neurotechx" target="_blank">
+            <i class="fa fa-linkedin"></i>
+          </a>
+        </li>
+
+        <li>
+          <a class="nav-icons" href="https://neurotechx.herokuapp.com/" target="_blank">
+            <i class="fa fa-slack"></i>
+          </a>
+        </li>
+
+        <li>
+          <a class="nav-icons" href="https://github.com/NeuroTechX/" target="_blank">
+            <i class="fa fa-github"></i>
+          </a>
+        </li>
+
+        </ul>
+    </div>
+
+  </div>
+</nav>
+
+<!-- Intro Header -->
+<header class="intro">
+  <div class="intro-body">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="slick-slider">
+
+            <div>
+              <img src="img/feature_img1.jpg" alt="img"></a>
+            </div>
+
+            <div>
+               <img src="img/feature_img2.jpg" alt="img"></a>
+            </div>
+
+            <div>
+              <img src="img/feature_img3.jpg" alt="img"></a>
+            </div>
+
+            <div>
+               <img src="img/feature_img6.jpg" alt="img"></a>
+            </div>
+
+            <div>
+              <img src="img/feature_img5.jpg" alt="img"></a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="center col-md-8 col-md-offset-2">
+
+          <div class="heading text-center">
+            <h4>NEUROTECH<span style="color:#23b3c6">EDU</span></h4>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh lorem, malesuada vel turpis eget, consectetur maximus velit. Ut gravida ligula a purus dignissim, eget ultricies est elementum. Proin felis turpis, eleifend at ex pulvinar, interdum malesuada lacus.
+          </p>
+
+        </div>
+      </div>
+    </div>
+</header>
+
+<section id="workshops" class="container noBG-section">
+
+	<?php acf_form_head(); ?>
+		<div id="primary" class="fp-content-area">
+			<main id="main" class="site-main" role="main">
+
+				<div class="entry-content">
+				<?php query_posts('category=workshops'); ?>
+
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php the_content(); ?>
+
+						<?php the_field('workshop_title'); ?>
+						<?php the_field('workshop_date_start'); ?>
+						<?php the_field('workshop_time_start'); ?>
+						<?php the_field('workshop_date_end'); ?>
+						<?php the_field('workshop_time_end'); ?>
+						<?php the_field('workshop_entry'); ?>
+						<?php the_field('workshop_cost'); ?>
+						<?php the_field('workshop_level'); ?>
+						<?php the_field('workshop_location')?>
+
+						<?php
+
+						$location = get_field('location');
+
+						if( !empty($location) ):
+						?>
+						<div class="acf-map">
+							<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+						</div>
+						<?php endif; ?>
+
+						<!--<iframe
+						  width="600"
+						  height="450"
+						  frameborder="0" style="border:0"
+						  src="https://www.google.com/maps/embed/v1/view
+						  ?key=YOUR_API_KEY
+						  &center=<?php the_field('workshop_location')?>
+						  &zoom=18
+						  &maptype=satellite" allowfullscreen>
+						</iframe> -->
+
+						<?php the_field('workshop_contact'); ?>
+						<?php the_field('workshop_summary'); ?>
+						<?php the_field('workshop_requirements'); ?>
+						<?php the_field('workshop_program'); ?>
+						<?php the_field('workshop_instructor'); ?>
+						<?php the_field('workshop_sponsors'); ?>
+					<?php endwhile; ?>
+
+				</div><!-- .entry-content -->
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+</section>
+
+
+<!--=================== ABOUT NEUROTECHX ====================-->
+
+<section id="aboutNTX" class="container noBG-section-dark">
+
+  <div class="row heading aboutntx-outer">
+
+    <div class="col-xs-6 col-xs-offset-1 text-content">
+      <div class="row">
+        <h4>About NeurotechX</h4>
+        <p>
+          NeuroTechX brings hackers, enthusiasts, researchers and experts together to drive innovation and foster collaboration at local and international scales. Our core mission is to build a strong global neurotech community by providing key resources, learning opportunities, and by being leaders in local and worldwide technological initiatives. We believe neurotechnology is key to better understanding and to improving who we are. Join us to take part in the conversation and help shape the future of neurotechnology!
+        </p>
+      </div>
+            <div>
+        <a class="btn btn-default btn-lg" href="http://eepurl.com/bEQDKX" target="_blank"><span>SUBSCRIBE TO THE </span>NEWSLETTER</a>
+      </div>
+    </div>
+
+    <div class="col-xs-4 right-col">
+
+      <h4 id="city-chapter">City Chapters</h4>
+      <div class="btn-group">
+        <button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose a City <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li>CANADA</li>
+          <li><a href='http://www.meetup.com/NeuroTechMTL' target="_blank">Montreal</a></li>
+          <li><a href='http://www.meetup.com/NeuroTechTO' target="_blank">Toronto</a></li>
+          <li><a href='http://www.meetup.com/NeuroTechVAN' target="_blank">Vancouver</a></li>
+
+          <li>UNITED STATES</li>
+          <li><a href='http://www.meetup.com/NeuroTechBOS' target="_blank">Boston</a></li>
+          <li><a href='http://www.meetup.com/NeuroTechLAX' target="_blank">Los Angeles</a></li>
+          <li><a href='http://www.meetup.com/NeuroTechNYC' target="_blank">New York City</a></li>
+          <li><a href='http://www.meetup.com/NeuroTechSF' target="_blank">San Francisco</a></li>
+
+          <li>NETHERLANDS</li>
+          <li><a href='http://www.meetup.com/NeuroTechAMS' target="_blank">Amsterdam</a></li>
+
+          <li>FRANCE</li>
+          <li><a href='http://www.meetup.com/NeuroTechPAR' target="_blank">Paris</a></li>
+
+          <li>SPAIN</li>
+          <li><a href='http://www.meetup.com/NeuroTechBCN' target="_blank">Barcelona</a></li>
+
+          <li>UNITED KINGDOM</li>
+          <li><a href='http://www.meetup.com/NeuroTechLDN' target="_blank">London</a></li>
+
+          <li>PERU</li>
+          <li><a href='http://www.meetup.com/NeuroTechLIMA' target="_blank">Lima</a></li>
+
+          <li>UNITED KINGDOM</li>
+          <li><a href='http://www.meetup.com/NeuroTechLDN' target="_blank">London</a></li>
+
+          <li>ISREAL</li>
+          <li><a href='http://www.meetup.com/NeuroTechTLV' target="_blank">Tel-Aviv</a></li>
+
+        </ul>
+      </div>
+
+
+      <h4 id="initiatives-header">Initiatives</h4>
+
+      <div class="btn-group">
+        <button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose a Project <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href='#WhereWeAre'>City Chapters</a></li>
+          <li><a href='http://news.neurotechx.com' target="_blank">NeurotechXNews</a></li>
+          <li><a href='http://neurotechx.com/studentclubs.html' target="_blank">Student Clubs</a></li>
+          <li><a href='http://neurotechx.com/studentclubs.html' target="_blank">Student Competition</a></li>
+        </ul>
+      </div>
+
+        <h4>
+          <a href="http://neurotechx.com/opportunities/index.html">OPPORTUNITIES</a>
+        </h4>
+
+        <h4>
+          <a href="http://neurotechx.com/resources/NeuroTechX_Media_Kit.pdf">DOWNLOAD MEDIA KIT</a>
+        </h4>
+
+        <h4>
+          <a href="mailto:partners@neurotechx.com">SPONSOR US</a>
+        </h4>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="row heading">
+    <div class="col-xs-6 col-xs-offset-1 social-icons-outer">
+      <div class="row social-media-icons">
+        <table>
+
+          <td class="social-btn">
+            <a href="https://facebook.com/neurotechx" target="_blank" class="btn btn-circle-small"><i class="fa fa-facebook animated-small"></i></a>
+          </td>
+
+          <td class="social-btn">
+            <a href="https://twitter.com/NeuroTechX" target="_blank" class="btn btn-circle-small"><i class="fa fa-twitter animated-small"></i></a>
+          </td>
+
+          <td class="social-btn">
+            <a href="https://www.linkedin.com/company/neurotechx" target="_blank" class="btn btn-circle-small"><i class="fa fa-linkedin animated-small"></i></a>
+          </td>
+
+          <td class="social-btn">
+            <a href="https://neurotechx.herokuapp.com/" target="_blank" class="btn btn-circle-small"><i class="fa fa-slack animated-small"></i></a>
+          </td>
+
+          <td class="social-btn">
+            <a href="https://github.com/NeuroTechX/" target="_blank" class="btn btn-circle-small"><i class="fa fa-github animated-small"></i></a>
+          </td>
+
+        </table>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+
+<!--- SCRIPTS AND APIS -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+
+<!-- Custom Theme JavaScript -->
+<script src="js/template.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="js/jquery.easing.min.js"></script>
+
+<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+
+<!--google analytics -->
+
+<!-- add/change your google analytics if needed.-->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65979944-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+ <!-- For smooth animatin  -->
+    <script src="js/wow.min.js"></script>
+    <!-- Easing scroll js-->
+      <script src="js/jquery.easing.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- superslides slider -->
+    <script src="js/jquery.superslides.min.js" type="text/javascript"></script>
+    <!-- slick slider -->
+    <script src="js/slick.min.js"></script>
+    <!-- for circle counter -->
+    <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
+    <!-- for portfolio filter gallery -->
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/elastic_grid.min.js"></script>
+    <script src="js/portfolio_slider.js"></script>
+
+    <!-- Custom js-->
+    <script src="js/custom.js"></script>
+
+    <script type="text/javascript">
+      $( document ).ready(function() {
+        $('.slick-slider').slick({
+          dots: true,
+          infinite: true,
+          speed: 1200,
+          slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true,
+          autoplay: true,
+        });
+      });
+    </script>
+
+</body>
+
+<!-- Footer -->
+<footer>
+  <div class="row heading">
+    <div class="col-xs-6 col-xs-offset-1 footer-outer">
+      <div class ="row" style="text-align:left;">
+        <p>Copyright &copy; NeuroTechX 2016</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</html>
